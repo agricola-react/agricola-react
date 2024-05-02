3126f458c28bca1c231bd73a471b64b698b34b15
kimheechan <kimhecan@rainist.com>
2024-05-02 20:01:03 +0900

    ci 추가

--- pages/agricola.tsx
+++ pages/agricola.tsx
@@ -1,6 +1,5 @@
 const AgricolaPage = () => {
-    return <div>아그리콜라 (5월31일까지 완성목표)</div>
-}
+  return <div>아그리콜라 (5월31일까지 완성목표)</div>;
+};
 
-
-export default AgricolaPage
+export default AgricolaPage;
