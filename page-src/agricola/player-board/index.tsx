cf9b5d5ed06760bc8a1baeacb850c64d75fa2309
minSsan <msp1125@hanyang.ac.kr>
2024-05-09 20:30:20 +0900

    feat: boardValue -> 전역 상태 전환 (오류 해결중)

--- page-src/agricola/player-board/index.tsx
+++ page-src/agricola/player-board/index.tsx
@@ -1,43 +1,21 @@
 import styled from '@emotion/styled';
-import { useEffect, useState } from 'react';
-import { ResourceType, Slot, SlotType } from './player-board.sub/slot';
-import { useRecoilValue } from 'recoil';
-import { playersState } from '@/shared/recoil';
-
-type BoardValue = {
-  type: SlotType;
-  resource: ResourceType;
-  count: number;
-};
+import { useEffect } from 'react';
+import { Slot } from './player-board.sub/slot';
+import { useRecoilState } from 'recoil';
+import { Player, SlotValue, playersState } from '@/shared/recoil';
+import { produce } from 'immer';
 
 type Props = {
   playerNumber: number;
 };
 
-const initBoard: BoardValue[] = new Array(15)
-  .fill({
-    type: null,
-    resource: null,
-    count: 1,
-  })
-  .map((value, index) => {
-    if (index === 5 || index === 10) {
-      return {
-        type: '방',
-        resource: '사람',
-        count: 1,
-      };
-    }
-    return value;
-  });
-
 function getUpdatedFarmerBoard(
-  playerBoard: BoardValue[],
+  playerSlots: SlotValue[],
   type: 'reduce' | 'fill' | 'increase'
-): BoardValue[] {
+): SlotValue[] {
   switch (type) {
     case 'fill':
-      return playerBoard.map(slot => {
+      return playerSlots.map(slot => {
         if (slot.type === '방')
           return {
             ...slot,
@@ -47,9 +25,9 @@ function getUpdatedFarmerBoard(
         return slot;
       });
     case 'increase':
-      playerBoard.some((slot, index) => {
+      playerSlots.some((slot, index) => {
         if (slot.type === '방' && slot.resource === null) {
-          playerBoard[index] = {
+          playerSlots[index] = {
             ...slot,
             resource: '사람',
             count: slot.count + 1,
@@ -57,11 +35,11 @@ function getUpdatedFarmerBoard(
           return true;
         }
       });
-      return [...playerBoard];
+      return [...playerSlots];
     case 'reduce':
-      playerBoard.some((slot, index) => {
+      playerSlots.some((slot, index) => {
         if (slot.type === '방' && slot.resource === '사람') {
-          playerBoard[index] = {
+          playerSlots[index] = {
             ...slot,
             resource: null,
             count: slot.count,
@@ -69,38 +47,62 @@ function getUpdatedFarmerBoard(
           return true;
         }
       });
-      return [...playerBoard];
+      return [...playerSlots];
     default:
-      return [...playerBoard];
+      return [...playerSlots];
   }
 }
 
 // TODO: useContext
-export const PlayerBoard = ({ playerNumber }: Props) => {
-  const [playerBoard, setPlayerBoard] = useState<BoardValue[]>(initBoard);
-  const players = useRecoilValue(playersState);
-  const owner = players.find(player => player.number === playerNumber);
+export const PlayerSlots = ({ playerNumber }: Props) => {
+  const [players, setPlayers] = useRecoilState(playersState);
+  const owner = players.find(player => player.number === playerNumber) as Player;
 
-  const boardFarmers = playerBoard.reduce((sum, cur) => {
-    if (cur.type === '방' && cur.resource === '사람') return sum + 1;
+  const playerSlots = owner.slots;
+
+  const boardFarmers = owner.slots.reduce((sum, cur) => {
+    if (cur.type === '방' && cur.resource === '사람') return sum + cur.count;
     return sum;
   }, 0);
 
   useEffect(() => {
     if (owner === undefined) return;
+    console.log(`${owner.name}'s boardFarmers >>>`, boardFarmers);
     // homeFarmer 값이 감소한 경우 -> homeFarmer를 방에서 없앤다
     if (owner.homeFarmer < boardFarmers) {
-      setPlayerBoard(getUpdatedFarmerBoard(playerBoard, 'reduce'));
+      setPlayers(
+        produce(_players => {
+          _players[playerNumber - 1].slots = getUpdatedFarmerBoard(
+            _players[playerNumber - 1].slots,
+            'reduce'
+          );
+        })
+      );
       return;
     }
+    console.log('owner >>>', owner);
     // 모두 복귀 -> homeFarmer를 모든 방에 넣는다
     if (owner.homeFarmer === owner.farmer) {
-      setPlayerBoard(getUpdatedFarmerBoard(playerBoard, 'fill'));
+      setPlayers(
+        produce(_players => {
+          _players[playerNumber - 1].slots = getUpdatedFarmerBoard(
+            _players[playerNumber - 1].slots,
+            'fill'
+          );
+        })
+      );
       return;
     }
     // homeFarmer 값이 증가한 경우 -> homeFarmer를 방에 추가
     if (owner.homeFarmer > boardFarmers) {
-      setPlayerBoard(getUpdatedFarmerBoard(playerBoard, 'increase'));
+      setPlayers(
+        produce(_players => {
+          _players[playerNumber - 1].slots = getUpdatedFarmerBoard(
+            _players[playerNumber - 1].slots,
+            'increase'
+          );
+        })
+      );
       return;
     }
   }, [owner?.homeFarmer, owner?.farmer]);
@@ -111,7 +113,7 @@ export const PlayerBoard = ({ playerNumber }: Props) => {
         <h4>{owner?.name} 보드</h4>
       </Title>
       <Wrapper>
-        {playerBoard.map((info, index) => (
+        {playerSlots.map((info, index) => (
           <Slot
             key={`slot${index}`}
             type={info.type}
--- pages/agricola.tsx
+++ pages/agricola.tsx
@@ -2,7 +2,7 @@ import { currentPlayerIndexState, playersState, roundState } from '@/shared/reco
 import styled from '@emotion/styled';
 import { CentralBoard } from 'page-src/agricola/central-board';
 import { Header } from 'page-src/agricola/header';
-import { PlayerBoard } from 'page-src/agricola/player-board';
+import { PlayerSlots } from 'page-src/agricola/player-board';
 import { calculateFeedingCount } from 'page-src/agricola/shared/utils/calculate-feeding-count';
 import { UserSection } from 'page-src/agricola/user-section';
 import { useEffect } from 'react';
@@ -71,7 +71,7 @@ const AgricolaPage = () => {
       </BoardWrapper>
       <PlayerBoardWrapper>
         {players.map(player => (
-          <PlayerBoard key={player.number} playerNumber={player.number} />
+          <PlayerSlots key={player.number} playerNumber={player.number} />
         ))}
       </PlayerBoardWrapper>
     </StyledBackground>
