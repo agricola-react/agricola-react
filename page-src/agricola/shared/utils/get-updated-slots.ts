cfa2c36b0eceb40acba1bb9655cb29456e5d94d0
minSsan <msp1125@hanyang.ac.kr>
2024-05-11 16:00:30 +0900

    chore: getUpdatedSlots 함수 분리

--- page-src/agricola/shared/utils/get-updated-slots.ts
+++ page-src/agricola/shared/utils/get-updated-slots.ts
@@ -1,5 +1,13 @@
 import { Player, SlotValue } from '@/shared/recoil';
 
+function isOwnersRoom(slot: SlotValue) {
+  return slot.type === '방' && slot.resource === '사람';
+}
+
+function isEmptyRoom(slot: SlotValue) {
+  return slot.type === '방' && slot.resource === null;
+}
+
 export function getUpdatedSlots(
   playerSlots: SlotValue[],
   type: 'reduce' | 'fill' | 'increase',
@@ -13,23 +21,22 @@ export function getUpdatedSlots(
         return [...playerSlots];
       }
       // 전체 방
-      const totalRoomCnt = playerSlots.reduce((acc, cur) => {
-        if (cur.type === '방') return acc + 1;
-        return acc;
-      }, 0);
+      // const totalRoomCnt = playerSlots.reduce((acc, cur) => {
+      //   if (cur.type === '방') return acc + 1;
+      //   return acc;
+      // }, 0);
 
-      if (totalRoomCnt < player.farmer) {
-        console.error(`[${type}] 가족 구성원 수가 방 개수보다 많습니다.`);
-        return [...playerSlots];
-      }
+      // if (totalRoomCnt < player.farmer) {
+      //   console.error(`[${type}] 가족 구성원 수가 방 개수보다 많습니다.`);
+      //   return [...playerSlots];
+      // }
 
       let cnt = 0;
-      // 1. 주인 있는 방 먼저 채우기
+      //* 1. 주인 있는 방 먼저 채우기
       playerSlots.forEach((slot, index) => {
-        if (slot.type === '방' && slot.resource === '사람') {
+        if (isOwnersRoom(slot)) {
           playerSlots[index] = {
             ...slot,
-            resource: '사람',
             count: 1,
           };
           cnt++;
@@ -37,17 +44,32 @@ export function getUpdatedSlots(
       });
       // 추가된 구성원이 없으면 그대로 리턴
       if (cnt === player.farmer) return [...playerSlots];
-      // 2. 주인 없는 방에 새로운 구성원 채우기
+
+      //* 2. 주인 없는 방에 새로운 구성원 채우기
       playerSlots.some((slot, index) => {
         if (cnt === player.farmer) return true;
         // 주인 없는 방
-        if (slot.type === '방' && slot.resource === null) {
+        if (isEmptyRoom(slot)) {
           playerSlots[index] = {
             ...slot,
             resource: '사람',
             count: 1,
           };
           cnt++;
+        }
+        return false;
+      });
+      // 모든 구성원을 채웠으면 리턴
+      if (cnt === player.farmer) return [...playerSlots];
+
+      //* 3. 급한 가족 늘리기로 구성원을 추가한 경우
+      playerSlots.some((slot, index) => {
+        if (isOwnersRoom(slot) && slot.count === 1) {
+          playerSlots[index] = {
+            ...slot,
+            count: 2,
+          };
+          return true;
         }
         return false;
       });
@@ -59,7 +81,7 @@ export function getUpdatedSlots(
     case 'increase':
       playerSlots.some((slot, index) => {
         // 주인이 없는 방이 존재하는 경우에만
-        if (slot.type === '방' && slot.resource === null) {
+        if (isEmptyRoom(slot)) {
           playerSlots[index] = {
             ...slot,
             resource: '사람',
@@ -74,10 +96,10 @@ export function getUpdatedSlots(
     case 'reduce':
       playerSlots.some((slot, index) => {
         // 주인이 있는 방 & 비어있지 않은 방이 있는 경우만
-        if (slot.type === '방' && slot.resource === '사람' && slot.count > 0) {
+        if (isOwnersRoom(slot) && slot.count > 0) {
           playerSlots[index] = {
             ...slot,
-            count: 0,
+            count: slot.count - 1,
           };
           return true;
         }
