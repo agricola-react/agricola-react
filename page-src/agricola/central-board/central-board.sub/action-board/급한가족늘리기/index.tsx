45554a26d9aa665f0d8a8b6d7397265da267e189
kimheechan <kimhecan@rainist.com>
2024-06-03 17:33:29 +0900

    update

--- page-src/agricola/central-board/central-board.sub/action-board/급한가족늘리기/index.tsx
+++ page-src/agricola/central-board/central-board.sub/action-board/급한가족늘리기/index.tsx
@@ -1,23 +1,15 @@
-import {
-  currentActionState,
-  currentPlayerIndexState,
-  playersState,
-  roundState,
-} from '@/shared/recoil';
+import { currentActionState, roundState } from '@/shared/recoil';
 import { MeepleChild } from '@/shared/resource/meeple-child';
 import styled from '@emotion/styled';
-import { produce } from 'immer';
 import { ActionContainer } from 'page-src/agricola/central-board/central-board.sub/action-board/shared/components/action-container';
 import { useCurrentPlayer } from 'page-src/agricola/shared/hooks/use-current-player';
 import { useEffect, useState } from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
 
 export const 급한가족늘리기 = () => {
   const round = useRecoilValue(roundState);
   const [isActive, setIsActive] = useState(false);
   const [selectedPlayerNumber, setSelectedPlayerNumber] = useState<number | undefined>();
-  const [players, setPlayers] = useRecoilState(playersState);
-  const currentPlayerIndex = useRecoilValue(currentPlayerIndexState);
 
   const action = useRecoilValue(currentActionState);
   const { currentPlayer } = useCurrentPlayer();
@@ -30,24 +22,7 @@ export const 급한가족늘리기 = () => {
 
     if (selectedPlayerNumber !== undefined) return;
 
-    setSelectedPlayerNumber(currentPlayer.number);
-
-    setPlayers(
-      produce(_players => {
-        _players[currentPlayerIndex].slots = _players[currentPlayerIndex].slots.map(slot => {
-          if (slot.type === null) {
-            return {
-              ...slot,
-              resource: '사람',
-              count: 1,
-            };
-          }
-
-          return slot;
-        });
-        _players[currentPlayerIndex].farmer += 1;
-      })
-    );
+    alert('정식버전에서 만나요!!');
   };
 
   useEffect(() => {
